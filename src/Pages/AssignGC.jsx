6b2e8d492cabf1874e59bf01fc11af6d52c2d118
luispaulo0 <193279@ids.upchiapas.edu.mx>
2023-01-18 10:31:16 -0600

    Vista Gerente2

---
+++ src/Pages/AssignGC.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import img2 from "../assets/img/clip-961.png";
+import css from "/src/assets/signupGerente.css"
+
+
+const AssignGC = () =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default AssignGC;
