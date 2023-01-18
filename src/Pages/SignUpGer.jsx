880549afdfb4c5931889a3c5d3cc7cc8e5a87917
Kevin Daniel Flores Nataren <kevin_nataren@hotmail.com>
2023-01-18 11:14:36 -0600

    general fixes

---
+++ src/Pages/SignUpGer.jsx
@@ -0,0 +1,42 @@
+import React from "react";;
+import "../assets/signupGerente.css";
+import img1 from "/src/assets/img/clip-104.png";
+
+const SignUpGer = ()=> {
+    return  (
+        <div>
+            <section className="divisor">
+            <div className="izquierda">
+                <h1 id="h1A">Hola Gerente</h1>
+                <h2 id="h2A">Aquí puedes crear tu usuario</h2>
+                <img className="Img-A" src={img1} />
+            </div>
+            <div className="derecha">
+                <h1 id="h1R">Registrate</h1>
+                <br/>
+                <section id="sect">
+                    <div className="mb-3 row">
+                        <div className="col-sm-10 inp ">
+                            <input type="text"  className="form-control form-control-lg nombres"  placeholder="Nombre"/>
+                            <input type="text"  className="form-control form-control-lg apellidos"  placeholder="Apellidos"/>
+                        </div>
+                    </div>
+                    <div className="mb-3 row">
+                        <div className="col-sm-10">
+                            <input type="text"  className="form-control form-control-lg inputsL"  placeholder="Usuario"/>
+                        </div>
+                    </div>
+                    <div className="mb-3 row">
+                        <div className="col-sm-10">
+                            <input type="password" className="form-control form-control-lg inputsL" placeholder="Password"  id="inputPassword "/>
+                        </div>
+                    </div>
+                    <button type="button" className="btn btn-cl">Registrar</button>
+                </section>
+
+            </div>
+            </section>
+        </div>
+    )
+}
+export default SignUpGer;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
-import Login from '../pages/Login';
-import Register from '../pages/Register';
+import SignUpGer from '../Pages/SignUpGer';
 
 const AppRouter = () => {
   return (
     <Router>
         <Routes>
-            <Route path='/register' element={<Register />}/>
-            <Route path='/login' element={<Login />}/>
+            <Route path='/signupGer' element={<SignUpGer />}/>
         </Routes>
     </Router>
   )
